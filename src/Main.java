7342c4badcc2fd2528804cf63589df54f7a88a8c
Максим <lonelymaks123@gmail.com>
2026-03-10 17:27:01 +0200

    mistakes4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
 
         // Задание номер 1
 
         System.out.println("Hello SkyPro!");
 
-        int age = 21;
+        int age = 15;
 
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
@@ -15,15 +18,14 @@ public class Main {
 
         //Задание номер 2
 
-        int airTemperature1 = 4;
+        int airTemperature1 = 6;
 
         if (airTemperature1 <= 5) {
 
-            System.out.println("На улице " + airTemperature1 + " шапку нужно одеть");
+            System.out.println("На улице холодно шапку нужно одеть");
 
-        } else if (airTemperature1 >= 5) {
-
-            System.out.println("На улице " + airTemperature1 + " градусов шапку можно не одевать");
+        } else if (airTemperature1 > 5) {
+            System.out.println("На улице тепло шапку можно не одевать");
         }
 
         // Задание номер 3
@@ -40,35 +42,22 @@ public class Main {
 
         if (years >= 2 && years <= 6) {
             System.out.println("Ему нужно ходить в Сад!");
-        } else {
-            System.out.println("Ему не нужно ходить в сад !");
-        }
-        if (years >= 7 && years <= 17) {
+        } else if (years >= 7 && years <= 17) {
             System.out.println("Ему нужно ходить в Школу!");
-        } else {
-            System.out.println("Ему не нужно ходить в Школу");
-        }
-        if (years >= 18 && years <= 24) {
+        } else if (years >= 18 && years <= 24) {
             System.out.println("Его место в Университете");
-        } else {
-            System.out.println("Ему не нужно ходить в Универ.");
-        }
-        if (years > 24) {
+        } else if (years > 24) {
             System.out.println("Он уже ходить на работу .");
         }
         // Задание номер 5
-        int kidYears = 16;
+        int kidYears = 13;
 
         if (kidYears <= 5) {
             System.out.println("Если возраст ребенка равен, то ему нельзя кататься на аттракционе");
-        }
-        if (kidYears >= 5 && kidYears <= 14) {
-            System.out.println("Если возраст ребенка равен то можно кататься на аттракционе в сопровождении зрослого ");
-        } else {
-            System.out.println("Если возраст ребенка равен то можно кататься на аттракционе в сопровождении зрослого ");
-        }
-        if (kidYears >= 14) {
-            System.out.println("Если возраст ребенка равен больше 14, то ему, можно кататься на аттракционе без сопровождения взрослого");
+        } else if (kidYears >= 5 && kidYears <= 14) {
+            System.out.println("Можно кататься на аттракционе в сопровождении зрослого ");
+        } else if (kidYears >= 14) {
+            System.out.println("Можно кататься на аттракционе без сопровождении зрослого ");
         }
         // Задача номер 6
         int capacityPassengers = 102;
@@ -79,11 +68,13 @@ public class Main {
         int totalPassengers = passengersSeats + standingPassengers;
         if (totalPassengers < capacityPassengers) {
             System.out.println("В вагоне есть еще " + (capacityPassengers - totalPassengers) + " свободных мест");
-        } else {
+        } else if (capacityPassengers > 102) {
             System.out.println("Вагон полностью забит, мест нет.");
+        } else {
+            System.out.println("Свободных сидячих мест осталось " + (seats - passengersSeats) + " ! стоячих мест осталось "
+                    + (standingCapacity - standingPassengers) + " !");
         }
-        System.out.println("Свободных сидячих мест осталось " + (seats - passengersSeats) + " ! стоячих мест осталось "
-                + (standingCapacity - standingPassengers) + " !");
+
         // Задание номер 7
         int one = 1;
         int two = 2;
@@ -92,29 +83,10 @@ public class Main {
             System.out.println("Первое число самое большое");
         } else if (two >= one && two >= three) {
             System.out.println("Второе число самое большое");
-        } else {
+        } else if (three >= one && three >= two){
             System.out.println("Третье число самое большое");
         }
 
+
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
